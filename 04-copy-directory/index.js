ec35c247de26bc03a9e1a6cb624a36ac193f38c8
Alena636 <alenahomich636@gmail.com>
2023-05-07 12:20:37 +0300

    feat: add solution for 4 task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,29 @@
+const { copyFile } = require('fs/promises') //импортируем нужные модули(фс используется для взаимодействия с ноде)
+const path = require('path');
+const fs = require('fs');
 
+const folderSource = path.join(__dirname, 'files') //пути к папкам
+const newFolder = path.join(__dirname, 'files-copy')
+async function copyFolder() { 
+    fs.mkdir(newFolder, {recursive: true}, (err) => {//создаем новую папку
+        if(err) {
+            throw err
+        } else {
+            console.log('Create folder "files-copy"')
+        }
+    })
+
+    fs.readdir(folderSource, (err, files) => { //читаем папку файлс и копируем в новую папку содержимое
+        if(err) {
+            throw err
+        }
+        files.forEach((file) => {
+            const fileDir = path.join(__dirname, 'files', file)
+            const newFileDir = path.join(__dirname, 'files-copy', file)
+            copyFile(fileDir, newFileDir)
+            console.log(file)
+        })
+    })
+}
+
+copyFolder()
