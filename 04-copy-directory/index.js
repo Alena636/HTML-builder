9cd3803baa93cee785fd9165476aa6bb9f9caf4e
Alena636 <alenahomich636@gmail.com>
2023-05-10 11:23:30 +0300

    fix: fix the bugs in 4 task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,29 +1,46 @@
-const { copyFile } = require('fs/promises') //импортируем нужные модули(фс используется для взаимодействия с ноде)
+//импортируем нужные модули(фс используется для взаимодействия с ноде)
 const path = require('path');
 const fs = require('fs');
 
 const folderSource = path.join(__dirname, 'files') //пути к папкам
 const newFolder = path.join(__dirname, 'files-copy')
-async function copyFolder() { 
-    fs.mkdir(newFolder, {recursive: true}, (err) => {//создаем новую папку
-        if(err) {
-            throw err
-        } else {
-            console.log('Create folder "files-copy"')
-        }
-    })
+async function copyFolder(source, output) { 
+   try {
+    await delFolder(output)
+   } catch {
+    console.log('Create folder files-copy')
+   } finally {
+    await createFolder(output)
+    const data  = await readFolder(source)
+    await copyFiles(data, source, output)
+   }
+    }
 
-    fs.readdir(folderSource, (err, files) => { //читаем папку файлс и копируем в новую папку содержимое
-        if(err) {
-            throw err
-        }
-        files.forEach((file) => {
-            const fileDir = path.join(__dirname, 'files', file)
-            const newFileDir = path.join(__dirname, 'files-copy', file)
-            copyFile(fileDir, newFileDir)
-            console.log(file)
-        })
-    })
+async function delFolder(folder) {
+    await fs.promises.rm(folder, {recursive: true})
 }
 
-copyFolder()
+async function createFolder(folder) {
+    fs.promises.mkdir(folder, {recursive: true})
+}
+
+async function readFolder(folder) {
+    const namesFile = await fs.promises.readdir(folder, {withFileTypes: true})
+return namesFile
+}
+
+async function copyFiles(namesFile, folderSource, newFolder) {
+    for (let file of namesFile) {
+        const fileSource = path.join(folderSource, file.name)
+        const fileOutput = path.join(newFolder, file.name)
+        if(file.isFile()) {
+            fs.promises.copyFile(fileSource, fileOutput)
+        } else {
+            await copyFolder(fileSource, fileOutput)
+        }
+    }
+}
+
+copyFolder(folderSource, newFolder)
+
+
